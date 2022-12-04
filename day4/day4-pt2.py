0e5f0329b7883fc6a2747b6252d8eeab77429e4b
Ivan De Dios <idedios@usc.edu>
2022-12-03 23:05:46 -0800

    Completed Day 4

---
+++ day4/day4-pt2.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Ivan De Dios - Day 4 part 2 Advent of Code 2022
+
+contain_counter = 0
+with open('assignments.txt', 'r') as at:
+    for pairs in at:
+        if pairs == '\n':
+            break
+        pairs = pairs.rstrip()
+        range1 = pairs.split(',')[0]
+        range2 = pairs.split(',')[1]
+        r1_lb = int(range1.split('-')[0])
+        r1_ub = int(range1.split('-')[1])
+        r2_lb = int(range2.split('-')[0])
+        r2_ub = int(range2.split('-')[1])
+        if ((r1_lb <= r2_lb and r1_ub >= r2_lb) or
+                (r2_lb <= r1_lb and r2_ub >= r1_lb)):
+            contain_counter += 1
+print(contain_counter)
